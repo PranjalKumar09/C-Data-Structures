2a28bc0f4e2f3b471acd42ee370b280e17e6a6ec
PranjalKumar09 <coderkumarshukla@gmail.com>
2024-08-06 16:12:20 +0530

    added the intro

---
+++ Recursion/intro.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+/*
+
+Recursion : function calling itself 
+* way to break complicated problems 
+Big type problem depend on same type function 
+
+Reursive : F(x) = G ( F(x-1) , F(x-2)  )
+base case : where for f(x) 
+           * must have return  
+           * tells where to stop 
+           * else segmentaion error 
+Recusrive relation -> Processing
+both mandotory recursive function and base case            
+
+
+Recusive tree -> a graphical representation that illustrates execution flow of a recursive function. It provides a visual breakdown of recursive calls, showcasing  progression of algorithm as it branches out and eventually reaches a base case. The tree structure helps in analyzing  time complexity and understanding  recursive process involved.
+           
+
+Head recursion 
+func (){
+    Base case
+    Recursive Call 
+    Processing
+}
+           
+Tail recursion 
+func (){
+    Base case
+    Processing
+    Recursive Call 
+}
+           
+           
+
+ex-> fn(arr+1 , size-1)
+             */
+
+int main() {}
