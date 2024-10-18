d048b510b1632a0a465eb4ff519396a550dfb8df
PranjalKumar09 <coderkumarshukla@gmail.com>
2024-10-18 10:00:00 +0530

    Add Backtracing Basics - Implement initial backtracking algorithm

---
+++ BackTracking/Backtracing_basics.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* 
+BackTracking Basics
+===================
+
+Best possible Solution
+    Uses recursive calling
+    Depth first search
+    Use to find feasible solution to decision problem
+                                    ----------------- 
+    Very found very effective optimization problem
+                            ---------------------
+*/
+
+int main(){}
