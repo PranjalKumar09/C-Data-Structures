bc44de2912fd1509ab78f91f0389efeb55fee7ad
PranjalKumar09 <coderkumarshukla@gmail.com>
2024-11-23 18:00:00 +0530

    Add miscellaneous OOPS code snippets

---
+++ OOPS/miscellaneous_codes.cpp
@@ -0,0 +1,15 @@
+/*
+    int &fun(){
+        static int x = 10;
+        return x;
+
+    }
+    int main(){
+        fun() = 30 ;
+        cout << fun() ; //output will 30
+        return 0 ;
+    }
+
+
+
+  */
