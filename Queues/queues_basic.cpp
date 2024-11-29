a56b8086cd22b663ebf28ef5f77f6f3e5cf34a59
PranjalKumar09 <coderkumarshukla@gmail.com>
2024-11-29 14:00:00 +0530

    Add basic queue operations

---
+++ Queues/queues_basic.cpp
@@ -0,0 +1,11 @@
+/* 
+QUEQUES
+A linaear data structure that structure that is opend at both end & operations are perfrormed first in first out
+fast & flexible
+
+    front                   back
+    ____________________________
+    |   |   |   |   |   |   |   |  enque
+    -----------------------------
+dequeu
+ */
