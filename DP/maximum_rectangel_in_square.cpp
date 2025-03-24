3f25d63412f81e20fa59187c77bb3bc26ed6fa98
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-24 11:16:39 +0530

    🧱 Maximal rectangle in binary matrix - improved brute force to optimized DP

---
+++ DP/maximum_rectangel_in_square.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* 
+    LARGEST SQUARE BOX
+
+
+    this can done by histogram logic
+    just do this assuming every row as base by this logic
+
+
+    is same problme in max rectangle in binary matrix
+ */
+
+
+
+
+int main(){
+
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+/* 
+    LARGEST SQUARE BOX
+
+
+    this can done by histogram logic
+    just do this assuming every row as base by this logic
+
+
+    is same problme in max rectangle in binary matrix
+ */
+
+
+
+
+int main(){
+
+}
